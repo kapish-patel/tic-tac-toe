f3c510f6dd36be0fffea0a27b8fa2d09d9f2b53a
Kapish-patel <kpatelkapish@gmail.com>
2023-12-14 16:53:55 -0800

    added logic for declaring winning player and draw

--- game.py
+++ game.py
@@ -17,7 +17,6 @@ RESOLVER = {
 # status enum class hold different status
 class status(Enum):
     PROGRESS = "game in progress..."
-    PENDING = "decision pending..."
     WIN_X = "Player X wins!"
     WIN_O = "Player O wins!"
     DRAW = "It's a draw!"
@@ -29,15 +28,29 @@ class TicTacToe:
         # value of grid, board and user
         self.grid = TicTacToe.generate_empty_grid()
         self.board = self.generate_board() 
+        self.winning_combinations = TicTacToe.generate_winning_combinations()
         
         self.users = [(users[0], 79),(users[1], 88)]
         self.game_status = status.PROGRESS
         self.current_player = 0
+        self.movecount = 1
 
         self.display_Users()
         self.display_Board()
         # start game
         self.game_start()
+
+    # function to generate combination or winning conditions
+    @staticmethod
+    def generate_winning_combinations():
+        return [[(0,0),(0,1),(0,2)],    # top row
+                [(1,0),(1,1),(1,2)],    # mid row
+                [(2,0),(2,1),(2,2)],    # bottom row
+                [(0,0),(1,0),(2,0)],    # left column
+                [(0,1),(1,1),(1,2)],    # mid column
+                [(0,2),(1,2),(2,2)],    # right column
+                [(0,0),(1,1),(2,2)],    # top left - bottom right diagonal
+                [(0,2),(1,1),(2,0)]]    # top right - bottom left diagonal
 
     # function to generate grid just the X's and O's
     @staticmethod
@@ -84,29 +97,42 @@ class TicTacToe:
     def update_board(self):
         self.board = self.generate_board()
 
+    # function to check who is winning
+    def check_win(self):
+        # the logic is there are 8 winning possibilities check each possibility with the current version of user
+        for combination in self.winning_combinations:   # combination from all the combinations
+            if all(self.grid[i][j] == self.users[self.current_player][1] for i,j in combination):   # if all the 3 cells are filled with respective X, O then change the status of game
+                self.game_status = status.WIN_O if self.current_player == 0 else status.WIN_X        
+        
     # function to update the grid with X and O
     def update_grid(self, i, j):
         self.grid[i][j] = self.users[self.current_player][1]
+        #check if any player is winning 
+        if self.movecount >= 3:
+            self.check_win()
         self.update_board()
         self.toogleplayer()
 
-        if not any(9608 in row for row in self.grid):
-            self.game_status = status.PENDING
+        if not any(9608 in row for row in self.grid):   # if the grid is full then change the status to draw
+            self.game_status = status.DRAW
 
     # function to take player input
     def playerInput(self) -> (int, int):
+            # print(f"input count:: {self.movecount}")
             cell_input = input("Enter the cell number :: ")
             cell_Number = int(cell_input)
             i,j = RESOLVER.get(cell_Number)
+            if self.current_player == 1:
+                self.movecount += 1
             return (i,j) if self.check_valid_input(i, j) else self.playerInput()    #return if input is valid
-
+    
     #fucntion to decide player to play
     def decidePlayer(self):
         if self.current_player == 0:
             print(f"Player 1, {self.users[0][0]} is playing[{chr(self.users[0][1])}] move...")
         else:
             print(f"Player 2, {self.users[1][0]} is playing[{chr(self.users[1][1])}] move...")
-        
+
     # fucntion to start the game    
     def game_start(self):
         while self.game_status == status.PROGRESS:
@@ -114,10 +140,10 @@ class TicTacToe:
             i,j = self.playerInput()
             self.update_grid(i, j)
             self.display_Board()
-        if self.game_status == status.PENDING:
-            print("decision pending..........")     # start working here make logic for decision making also think of updating the update grid function think of real testing 
+        print(self.game_status)
         
 
+# user class takes user name and randomly decide first player
 class User:
     def __init__(self) -> None:
         self.users = ['','']
