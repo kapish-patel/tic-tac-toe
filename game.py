79254f918f6a96552eeb8af64f0425e29951bac5
Kapish-patel <kpatelkapish@gmail.com>
2023-12-16 09:38:50 -0800

    final version with formatting and comments

--- game.py
+++ game.py
@@ -1,18 +1,79 @@
 '''
 Todo: make a python script for playing tic - tac - toe game 
 Author: Kapish Patel
+
+How to run the game:
+1. install python 
+2. open the directory 
+3. and run python3 game.py 
+
+sample:
+1. Run the command 
+    python3 game.py
+2. Enter usernames
+    Enter Name of First Player :: kapish
+    Enter Name of Second Player :: parth
+3. loading screen
+    Randomly selecting first player...
+    \
+4. Start playing game by entering cell numbers in input starting from 1-9
+    Player 1: a, Player 2: s
+    ┌─┬─┬─┐
+    │1│2│3│
+    ├─┼─┼─┤
+    │4│5│6│
+    ├─┼─┼─┤
+    │7│8│9│
+    └─┴─┴─┘
+    Player 1, a is playing[O] move...
+    Enter the cell number :: 
+5. Results
+    Draw match Output
+    ┌─┬─┬─┐
+    │O│X│O│
+    ├─┼─┼─┤
+    │X│X│O│
+    ├─┼─┼─┤
+    │O│O│X│
+    └─┴─┴─┘
+    
+    O WINS Output
+    ┌─┬─┬─┐
+    │O│O│O│
+    ├─┼─┼─┤
+    │X│X│O│
+    ├─┼─┼─┤
+    │O│X│X│
+    └─┴─┴─┘
+    
+    X WINS Output
+    ┌─┬─┬─┐
+    │X│X│O│
+    ├─┼─┼─┤
+    │O│X│O│
+    ├─┼─┼─┤
+    │O│O│X│
+    └─┴─┴─┘
 '''
+
+# import statements 
+# random for generating random numbers
+# os for clearing screen
+# time for sleep function
+# enum for enum class
 import random
 import os
 import time
 from enum import Enum
 
-
+# resolver dictionary which resolve cell number to cell location
 RESOLVER = {
     1: (0,0), 2:(0,1), 3:(0,2),
     4: (1,0), 5:(1,1), 6:(1,2),
     7: (2,0), 8:(2,1), 9:(2,2),
 }
+
+
 
 # status enum class hold different status
 class status(Enum):
@@ -22,21 +83,21 @@ class status(Enum):
     DRAW = "It's a draw!"
 
 
+
 # tic tac toe class which hold all the operations from user
 class TicTacToe:
     def __init__(self, users) -> None:
-        # value of grid, board and user
+        # value of grid, board, and 8 winning conditions
         self.grid = TicTacToe.generate_empty_grid()
         self.board = self.generate_board() 
         self.winning_combinations = TicTacToe.generate_winning_combinations()
         
+        # value of user, game status and move count
         self.users = [(users[0], 79),(users[1], 88)]
-        self.game_status = status.PROGRESS
-        self.current_player = 0
+        self.game_status = status.PROGRESS.value
+        self.current_player = 0 # 0 for player 1, 1 for player 2
         self.movecount = 1
 
-        self.display_Users()
-        self.display_Board()
         # start game
         self.game_start()
 
@@ -102,7 +163,7 @@ class TicTacToe:
         # the logic is there are 8 winning possibilities check each possibility with the current version of user
         for combination in self.winning_combinations:   # combination from all the combinations
             if all(self.grid[i][j] == self.users[self.current_player][1] for i,j in combination):   # if all the 3 cells are filled with respective X, O then change the status of game
-                self.game_status = status.WIN_O if self.current_player == 0 else status.WIN_X        
+                self.game_status = status.WIN_O.value if self.current_player == 0 else status.WIN_X.value        
         
     # function to update the grid with X and O
     def update_grid(self, i, j):
@@ -114,7 +175,7 @@ class TicTacToe:
         self.toogleplayer()
 
         if not any(9608 in row for row in self.grid):   # if the grid is full then change the status to draw
-            self.game_status = status.DRAW
+            self.game_status = status.DRAW.value
 
     # function to take player input
     def playerInput(self) -> (int, int):
@@ -133,14 +194,53 @@ class TicTacToe:
         else:
             print(f"Player 2, {self.users[1][0]} is playing[{chr(self.users[1][1])}] move...")
 
+    # function to display pattern
+    @staticmethod
+    def display_pattern():
+        print("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*")
+
+    # function to display draw decision
+    def display_draw(self):
+        TicTacToe.display_pattern()
+        print(f"{status.DRAW.value} well played {self.users[0][0]}, {self.users[1][0]}")
+        TicTacToe.display_pattern()
+    
+    # function to diaplay winning player
+    def display_win(self,char):
+        TicTacToe.display_pattern()
+        if char == 'X':
+            print(f"{status.WIN_X.value} well played {self.users[1][0]}")
+        else:
+            print(f"{status.WIN_O.value} well played {self.users[0][0]}")
+        TicTacToe.display_pattern()
+
+
+    # function to display decision of the game
+    def display_decision(self):
+        self.display_Board()
+        print(self.game_status)
+        if self.game_status == status.DRAW.value:
+            self.display_draw()
+        elif self.game_status == status.WIN_O.value:
+           self.display_win('O')
+        else:
+            self.display_win('X')
+        
     # fucntion to start the game    
     def game_start(self):
-        while self.game_status == status.PROGRESS:
+        self.display_Users()
+        self.display_Board()
+        while self.game_status == status.PROGRESS.value:  #repeat this process till status changes
             self.decidePlayer()
             i,j = self.playerInput()
+            os.system('clear')
             self.update_grid(i, j)
             self.display_Board()
-        print(self.game_status)
+        #print results
+        os.system('clear')
+        #call function which fancily print winner
+        self.display_decision()
+
         
 
 # user class takes user name and randomly decide first player
@@ -150,17 +250,16 @@ class User:
         self.userInputs()
         self.random_select_firstplayer()
 
+    # function for username input
     def userInputs(self):
         self.users[0] = input("Enter Name of First Player :: ")
         self.users[1] = input("Enter Name of Second Player :: ")    
 
+    # function which retuns players data
     def get_players(self):
         return self.users
     
-    @staticmethod
-    def test_print(i):
-        print(chr(i), end=" ")
-    
+    # function to display loading screen
     @staticmethod
     def displayLoading():
         for i in range(0,10):
@@ -168,6 +267,7 @@ class User:
             User.printSleep('/')
             User.printSleep('-')
 
+    # function to print and sleep
     @staticmethod
     def printSleep(char):
         print("Randomly selecting first player...")
@@ -175,12 +275,14 @@ class User:
         time.sleep(0.1)
         os.system('clear')
 
+    # function to randomly select first player
     def random_select_firstplayer(self):
         User.displayLoading()
         randomNumber = random.randint(0,1)
         if randomNumber == 1:
             self.swapPlayer()
 
+    # function to swap player
     def swapPlayer(self):
         temp = self.users[0]
         self.users[0] = self.users[1]
